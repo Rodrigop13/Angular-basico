8ac83d00fd684daeaaed7b0f28af4266e84486d7
victor rodrigo puc estrella <vrpuc@aspel.com.mx>
2022-12-09 18:39:05 -0600

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesModules } from './heroes/heroes.module';
+import { ContadorModules } from './contador/contador.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        HeroesModules,
+        ContadorModules
+    ]
 })
 export class AppModule { }
---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core'
+@Component({
+    selector:'app-contador',
+    template:`
+        <h1>{{titulo}}</h1>
+        <h1>La base es: <strong>5</strong></h1>
+        <button (click)="acumular(base)">+{{base}}</button>
+        <span>{{numero}}</span>
+        <button (click)="acumular(-base)">-{{base}}</button>
+    `
+})
+export class ContadorComponent{
+    titulo = 'Contadpr App';
+    numero: number = 10;
+    base: number =5;
+    acumular(valor: number){
+        this.numero+=valor;
+    }
+}
